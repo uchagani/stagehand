b41adceb4537e204d58a9d21301846e9d4b85949
Umair Chagani <umair.chagani@gmail.com>
2022-02-08 22:25:13 -0500

    Added Under annotation

--- src/test/java/io/github/uchagani/stagehand/pages/PageWithoutConstructor.java
+++ src/test/java/io/github/uchagani/stagehand/pages/PageWithoutConstructor.java
@@ -3,6 +3,7 @@ package io.github.uchagani.stagehand.pages;
 import com.microsoft.playwright.Locator;
 import io.github.uchagani.stagehand.annotations.Find;
 import io.github.uchagani.stagehand.annotations.PageObject;
+import io.github.uchagani.stagehand.annotations.Under;
 
 @PageObject
 public class PageWithoutConstructor {
@@ -12,4 +13,25 @@ public class PageWithoutConstructor {
     @Find(".paragraph")
     public Locator paragraph;
 
+    @Find("#firstNameFormDiv")
+    public Locator firstNameFormDiv;
+
+    @Find("form")
+    @Under({"firstNameFormDiv"})
+    public Locator firstNameForm;
+
+    @Find("input")
+    @Under({"firstNameForm"})
+    public Locator firstNameInput;
+
+    @Find("#lastNameFormDiv")
+    public Locator lastNameFormDiv;
+
+    @Find("form")
+    @Under({"lastNameFormDiv"})
+    public Locator lastNameForm;
+
+    @Find("input")
+    @Under({"lastNameFormDiv", "lastNameForm"})
+    public Locator lastNameInput;
 }
